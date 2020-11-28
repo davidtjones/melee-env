788e557aa0515d5a176b871a13c76d542ae48ead
David Jones <d.t.jones@outlook.com>
2020-11-28 00:13:35 -0500

    cleanup

--- src/ai/models/a2c/run.py
+++ src/ai/models/a2c/run.py
@@ -15,7 +15,7 @@ from src.ai.models.a2c.training import train, test
 
 if __name__ == '__main__':
     episodes = 20
-    discount_factor = .25
+    discount_factor = .1
     learning_rate = 0.001
 
     gpu = torch.device('cuda:0')
@@ -34,19 +34,20 @@ if __name__ == '__main__':
     test_rewards = []
 
     for episode in range(episodes):
+        print("Training")
         p_loss, v_loss, train_reward = train(env, policy, optimizer, discount_factor, cpu)
+        print("Testing")
         test_reward = test(env, policy, cpu)
  
     env.close()
-    print("training complete") 
 
-    # plt.title(f"Learning Rate")
-    # plt.xlabel("Episode")
-    # plt.ylabel("Reward")
-    # plt.plot(train_rewards, label='train')
-    # plt.plot(test_rewards, label='test')
-    # plt.legend()
-    # plt.show()
+    plt.title(f"Learning Rate")
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+    plt.plot(train_rewards, label='train')
+    plt.plot(test_rewards, label='test')
+    plt.legend()
+    plt.show()
     print("Complete")
     sys.exit(0)
 
